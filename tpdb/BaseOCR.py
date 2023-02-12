9b3421e021472ee872e2ce60af7537554ca05fed
DirtyRacer1337 <DirtyRacer@ya.ru>
2023-02-12 09:58:13 +0700

    Minor changes

--- tpdb/BaseOCR.py
+++ tpdb/BaseOCR.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 from typing import Any
 
 import pytesseract
@@ -7,7 +8,8 @@ from unidecode import unidecode
 
 
 class BaseOCR:
-    def get_data_from_image(self, image: Image.Image) -> Any:
+    def get_data_from_image(self, image: bytes) -> Any:
+        image = self._get_image_from_bytes(image)
         image = self._image_pre_processing(image)
         text: str = pytesseract.image_to_string(image)
         res = self._text_post_processing(text)
@@ -28,3 +30,8 @@ class BaseOCR:
         text = text.strip()
 
         return text
+
+    @staticmethod
+    def _get_image_from_bytes(data: bytes):
+        data = BytesIO(data)
+        return Image.open(data)
